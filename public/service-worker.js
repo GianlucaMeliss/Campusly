476d2a1bb01e1e1b351e846cc601590cb3709ffa
gianluca <melis.gianlucagm@gmail.com>
2026-09-25 15:15:23 +0200

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // Cambia questa riga:
-const CACHE_NAME = 'campusly-app-v11'; 
-const DATA_CACHE_NAME = 'campusly-data-v11';
+const CACHE_NAME = 'campusly-app-v12'; 
+const DATA_CACHE_NAME = 'campusly-data-v12';
 const PATH_PREFIX = '/Campusly/';
 
 const ASSETS_TO_CACHE = [
